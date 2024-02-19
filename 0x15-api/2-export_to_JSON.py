52dbfb437e5e86ce0c4cc8b8e08598233728aea5
mistidevs <mistidevs@gmail.com>
2024-02-19 10:49:03 -0800

    More handlers

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,33 +1,19 @@
 #!/usr/bin/python3
-"""
-Using an API to obtain data
-"""
+"""Exports to-do list information for a given employee ID to JSON format."""
 import json
 import requests
-from sys import argv
+import sys
 
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-if __name__ == '__main__':
-    """
-    Returns information about his/her TODO list progress.
-    Exports in JSON format.
-    """
-    user_id = argv[1]
-    url_todos = f"https://jsonplaceholder.typicode.com/users/{argv[1]}/todos"
-    url_name = f"https://jsonplaceholder.typicode.com/users/{argv[1]}"
-    response_todos = requests.get(url_todos)
-    response_name = requests.get(url_name)
-    todos = response_todos.json()
-    name = (name := response_name.json()).get('name')
-    tasks_num = len(todos)
-
-    all_tasks = {f"{user_id}": []}
-    for i in range(tasks_num):
-        title = todos[i]['title']
-        status = todos[i]['completed']
-        all_tasks[f"{user_id}"].append({"task": f"{title}",
-                                        "completed": f"{status}",
-                                        "username": f"{name}"})
-
-    with open(f"{user_id}.json", mode="w", encoding="utf-8") as f:
-        json.dump(all_tasks, f)
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": username
+            } for t in todos]}, jsonfile)
