cfd1f5075e4409847fb3c7a2cff2331a0826a810
mistidevs <mistidevs@gmail.com>
2024-03-06 10:07:06 -0800

    Using different implementation with sed

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,17 +1,6 @@
-# Fixes Apache error 500 by adding an index.html file
-file { '/var/www/html/index.html':
-  ensure  => present,
-  content => '
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Simple HTML Page</title>
-</head>
-<body>
-    <h1>It is all good</h1>
-</body>
-</html>',
+# automated puppet fix (to find out why Apache is returning a 500 error)
+
+exec { 'Fix wordpress site':
+  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  provider => shell,
 }
