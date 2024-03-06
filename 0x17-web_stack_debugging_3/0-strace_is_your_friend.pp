5ea75f8344d4d1d404ee71a41c4a2b1f9f933ec2
mistidevs <mistidevs@gmail.com>
2024-03-06 09:56:32 -0800

    Fixing Apache server erorr 500

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,17 @@
+# Fixes Apache error 500 by adding an index.html file
+file { '/var/www/html/index.html':
+  ensure  => present,
+  content => '
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple HTML Page</title>
+</head>
+<body>
+    <h1>It is all good</h1>
+</body>
+</html>',
+}
